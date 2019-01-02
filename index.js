65bcb6db0ac291355769d1ae3323ad4c92502e79
Talia Ritholtz <talia.ritholtz@baruchmail.cuny.edu>
2019-01-02 15:19:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var funkyFunction = function() {
     return "FUNKY!"
   }
 }
-console.log(funkyFunction())
+console.log(funkyFunction()())
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-theFunk = funkyFunction() 
+var theFunk = funkyFunction()
